1405a614a4cd6d526897d4e1424ab3d81f490e3e
Misha <mykahailo.kaschiy@gmail.com>
2020-05-14 12:01:23 +0300

    End Lesson 6

---
+++ Tic Tac Toe/Assets/Scripts/GameController.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public int whoseTurn;
+    public int turnCount;
+    public GameObject[] turnIcons;
+    public Sprite[] playIcons;
+    public Button[] tictactoeSpaces;
+    public int[] markedSpaces;
+    public Text WinnerText;
+    public GameObject[] winningLine;
+    public GameObject winnerPanel;
+
+    private void Start()
+    {
+        GameSetup();
+    }
+
+    void GameSetup()
+    {
+        whoseTurn = 0;
+        turnCount = 0;
+
+        turnIcons[0].SetActive(true);
+        turnIcons[1].SetActive(false);
+        
+        for (int i = 0; i < tictactoeSpaces.Length; i++)
+        {
+            tictactoeSpaces[i].interactable = true;
+            tictactoeSpaces[i].GetComponent<Image>().sprite = null;
+        }
+
+        for (int i = 0; i < markedSpaces.Length; i++)
+        {
+            markedSpaces[i] = -1;
+        }
+    }
+
+    public void TicTacToeButton(int WhichNumber)
+    {
+        tictactoeSpaces[WhichNumber].image.sprite = playIcons[whoseTurn];
+        tictactoeSpaces[WhichNumber].interactable = false;
+
+        markedSpaces[WhichNumber] = whoseTurn + 1;
+        turnCount++;
+
+        if(turnCount > 4)
+        {
+            WinnerCheck();
+        }
+
+        if(whoseTurn == 0)
+        {
+            whoseTurn = 1;
+
+            turnIcons[0].SetActive(false);
+            turnIcons[1].SetActive(true);
+        }
+        else
+        {
+            whoseTurn = 0;
+
+            turnIcons[0].SetActive(true);
+            turnIcons[1].SetActive(false);
+        }
+    }
+
+    void WinnerCheck()
+    {
+        int s1 = markedSpaces[0] + markedSpaces[1] + markedSpaces[2];
+        int s2 = markedSpaces[3] + markedSpaces[4] + markedSpaces[5];
+        int s3 = markedSpaces[6] + markedSpaces[7] + markedSpaces[8];
+        int s4 = markedSpaces[0] + markedSpaces[3] + markedSpaces[6];
+        int s5 = markedSpaces[1] + markedSpaces[4] + markedSpaces[7];
+        int s6 = markedSpaces[2] + markedSpaces[5] + markedSpaces[8];
+        int s7 = markedSpaces[0] + markedSpaces[4] + markedSpaces[8];
+        int s8 = markedSpaces[0] + markedSpaces[4] + markedSpaces[6];
+        int s9 = markedSpaces[2] + markedSpaces[4] + markedSpaces[6];
+
+        var solutions = new int[] { s1, s2, s3, s4, s5, s6, s7, s8, s9 };
+
+        for(int i = 0; i < solutions.Length; i++)
+        {
+            if(solutions[i] == 3*(whoseTurn + 1))
+            {
+                WinnerDisplay(i);
+                return;
+            }
+        }
+    }
+
+    void WinnerDisplay(int indexIn)
+    {
+        winnerPanel.gameObject.SetActive(true);
+
+        if (whoseTurn == 0)
+        {
+            WinnerText.text = "Player X Wins!";
+        }
+        else if(whoseTurn == 1)
+        {
+            WinnerText.text = "Player O Wins!";
+        }
+
+        winningLine[indexIn].SetActive(true);
+
+        for(int i = 0; i < tictactoeSpaces.Length; i++)
+        {
+            tictactoeSpaces[i].interactable = false;
+        }
+    }
+}
